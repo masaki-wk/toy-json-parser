7b7f5f7499b7a1b7d8e0b23b712f260417248272
Masaki Wakabayashi <masaki.wakabayashi@gmail.com>
2026-07-08 22:27:45 +0900

    Lexer: Rename helper function for clarity

--- tests/lexer.rs
+++ tests/lexer.rs
@@ -2,7 +2,7 @@ use anyhow::{Context as _, Result};
 
 use toy_json_parser::{CodePos, Lexer, TokenKind};
 
-fn do_tokenize_single(input: &str, expected_kind: TokenKind, expected_line: usize, expected_column: usize) -> Result<()> {
+fn do_tokenize_single_token(input: &str, expected_kind: TokenKind, expected_line: usize, expected_column: usize) -> Result<()> {
     let mut lexer = Lexer::new(input.chars());
     let token = lexer.next().with_context(|| "")?;
     let expected_pos = CodePos {
@@ -23,77 +23,77 @@ fn new() -> Result<()> {
 
 #[test]
 fn tokenize_left_bracket() -> Result<()> {
-    do_tokenize_single("[", TokenKind::LeftBracket, 1, 1)
+    do_tokenize_single_token("[", TokenKind::LeftBracket, 1, 1)
 }
 
 #[test]
 fn tokenize_right_bracket() -> Result<()> {
-    do_tokenize_single("]", TokenKind::RightBracket, 1, 1)
+    do_tokenize_single_token("]", TokenKind::RightBracket, 1, 1)
 }
 
 #[test]
 fn tokenize_left_brace() -> Result<()> {
-    do_tokenize_single("{", TokenKind::LeftBrace, 1, 1)
+    do_tokenize_single_token("{", TokenKind::LeftBrace, 1, 1)
 }
 
 #[test]
 fn tokenize_right_brace() -> Result<()> {
-    do_tokenize_single("}", TokenKind::RightBrace, 1, 1)
+    do_tokenize_single_token("}", TokenKind::RightBrace, 1, 1)
 }
 
 #[test]
 fn tokenize_colon() -> Result<()> {
-    do_tokenize_single(":", TokenKind::Colon, 1, 1)
+    do_tokenize_single_token(":", TokenKind::Colon, 1, 1)
 }
 
 #[test]
 fn tokenize_comma() -> Result<()> {
-    do_tokenize_single(",", TokenKind::Comma, 1, 1)
+    do_tokenize_single_token(",", TokenKind::Comma, 1, 1)
 }
 
 #[test]
 fn tokenize_true() -> Result<()> {
-    do_tokenize_single("true", TokenKind::Boolean(true), 1, 1)
+    do_tokenize_single_token("true", TokenKind::Boolean(true), 1, 1)
 }
 
 #[test]
 fn tokenize_false() -> Result<()> {
-    do_tokenize_single("false", TokenKind::Boolean(false), 1, 1)
+    do_tokenize_single_token("false", TokenKind::Boolean(false), 1, 1)
 }
 
 #[test]
 fn tokenize_null() -> Result<()> {
-    do_tokenize_single("null", TokenKind::Null, 1, 1)
+    do_tokenize_single_token("null", TokenKind::Null, 1, 1)
 }
 
 #[test]
 fn skip_space() -> Result<()> {
-    do_tokenize_single(" :", TokenKind::Colon, 1, 2)
+    do_tokenize_single_token(" :", TokenKind::Colon, 1, 2)
 }
 
 #[test]
 fn skip_tab() -> Result<()> {
-    do_tokenize_single("\t:", TokenKind::Colon, 1, 2)
+    do_tokenize_single_token("\t:", TokenKind::Colon, 1, 2)
 }
 
 #[test]
 fn skip_line_feed() -> Result<()> {
-    do_tokenize_single("\n:", TokenKind::Colon, 2, 1)
+    do_tokenize_single_token("\n:", TokenKind::Colon, 2, 1)
 }
 
 #[test]
 fn skip_carrige_return() -> Result<()> {
-    do_tokenize_single("\r:", TokenKind::Colon, 1, 2)
+    do_tokenize_single_token("\r:", TokenKind::Colon, 1, 2)
 }
 
 #[test]
 fn tokenize_invalid_char() -> Result<()> {
     let s = ".";
-    do_tokenize_single(s, TokenKind::Invalid(s.to_string()), 1, 1)
+    do_tokenize_single_token(s, TokenKind::Invalid(s.to_string()), 1, 1)
 }
 
 #[test]
 fn tokenize_invalid_raw_string() -> Result<()> {
     let s = "invalid";
-    do_tokenize_single(s, TokenKind::Invalid(s.to_string()), 1, 1)
+    do_tokenize_single_token(s, TokenKind::Invalid(s.to_string()), 1, 1)
 }
