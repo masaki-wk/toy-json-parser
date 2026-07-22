dc08a8b4b2430eb3880e0079a998fb7acaafd6e0
Masaki Wakabayashi <masaki.wakabayashi@gmail.com>
2026-07-23 08:46:10 +0900

    Refactor parser to return token range alongside parsed values to prepare to avoid using `lexer.position()`

--- src/parser.rs
+++ src/parser.rs
@@ -35,15 +35,15 @@ where
 
     /// Parses a code.
     pub fn parse(&mut self) -> Result<Value, ParserError> {
-        let value = self.parse_value()?;
+        let (value, _) = self.parse_value()?;
         match self.lexer.next() {
             Some(token) => Err(ParserError::ExtraTokenAtTheEnd(token.range.start)),
             None => Ok(value),
         }
     }
 
-    // Parses a value.
-    fn parse_value(&mut self) -> Result<Value, ParserError> {
+    // Parses a value and then returns the value and the range of the last token.
+    fn parse_value(&mut self) -> Result<(Value, Range<CodePos>), ParserError> {
         enum TokenCategory {
             BeginArray,
             BeginObject,
@@ -65,25 +65,32 @@ where
             TokenCategory::BeginObject => self.parse_rest_of_object(token_range),
             TokenCategory::Literal(lit) => {
                 let kind = ValueKind::Literal(lit);
-                Ok(Value { kind, range: token_range })
+                Ok((
+                    Value {
+                        kind,
+                        range: token_range.clone(),
+                    },
+                    token_range,
+                ))
             }
         }
     }
 
     // Parses a rest of the array.
-    fn parse_rest_of_array(&mut self, begin_array_token_range: Range<CodePos>) -> Result<Value, ParserError> {
+    fn parse_rest_of_array(&mut self, begin_array_token_range: Range<CodePos>) -> Result<(Value, Range<CodePos>), ParserError> {
         let mut buf: Vec<Box<Value>> = Vec::new();
-        let end = loop {
+        let (end, last_token_range) = loop {
             let token_pos = self.lexer.position();
             let token = self
                 .lexer
                 .peek()
                 .ok_or(ParserError::UnfinishedArray(begin_array_token_range.start, token_pos))?;
+            let token_range = token.range.clone();
             match token.kind {
                 TokenKind::Delimiter(Delimiter::RightBracket) => {
-                    let end = token.range.end;
+                    let end = token_range.end;
                     self.lexer.next();
-                    break end;
+                    break (end, token_range);
                 }
                 TokenKind::Delimiter(Delimiter::Comma) => {
                     if buf.is_empty() {
@@ -95,32 +102,36 @@ where
                 }
                 _ => Ok(()),
             }?;
-            let item = self.parse_value().map_err(|e| match e {
+            let (item, _) = self.parse_value().map_err(|e| match e {
                 ParserError::NoToken => ParserError::UnfinishedArray(begin_array_token_range.start, self.lexer.position()),
                 _ => e,
             })?;
             buf.push(Box::new(item));
         };
-        Ok(Value {
-            kind: ValueKind::Array(buf),
-            range: begin_array_token_range.start..end,
-        })
+        Ok((
+            Value {
+                kind: ValueKind::Array(buf),
+                range: begin_array_token_range.start..end,
+            },
+            last_token_range,
+        ))
     }
 
     // Parses a rest of the object.
-    fn parse_rest_of_object(&mut self, begin_object_token_range: Range<CodePos>) -> Result<Value, ParserError> {
+    fn parse_rest_of_object(&mut self, begin_object_token_range: Range<CodePos>) -> Result<(Value, Range<CodePos>), ParserError> {
         let mut buf: Vec<((String, Range<CodePos>), Box<Value>)> = Vec::new();
-        let end = loop {
+        let (end, last_token_range) = loop {
             let token_pos = self.lexer.position();
             let token = self
                 .lexer
                 .peek()
                 .ok_or(ParserError::UnfinishedObject(begin_object_token_range.start, token_pos))?;
+            let token_range = token.range.clone();
             match token.kind {
                 TokenKind::Delimiter(Delimiter::RightBrace) => {
-                    let end = token.range.end;
+                    let end = token_range.end;
                     self.lexer.next();
-                    break end;
+                    break (end, token_range);
                 }
                 TokenKind::Delimiter(Delimiter::Comma) => {
                     if buf.is_empty() {
@@ -132,17 +143,20 @@ where
                 }
                 _ => Ok(()),
             }?;
-            let pair = self.parse_pair_for_object(begin_object_token_range.clone())?;
-            buf.push((pair.0, Box::new(pair.1)));
+            let (name_pair, value, _) = self.parse_pair_for_object(begin_object_token_range.clone())?;
+            buf.push((name_pair, Box::new(value)));
         };
-        Ok(Value {
-            kind: ValueKind::Object(buf),
-            range: begin_object_token_range.start..end,
-        })
+        Ok((
+            Value {
+                kind: ValueKind::Object(buf),
+                range: begin_object_token_range.start..end,
+            },
+            last_token_range,
+        ))
     }
 
     // Parses a pair of the object.
-    fn parse_pair_for_object(&mut self, begin_object_token_range: Range<CodePos>) -> Result<((String, Range<CodePos>), Value), ParserError> {
+    fn parse_pair_for_object(&mut self, begin_object_token_range: Range<CodePos>) -> Result<((String, Range<CodePos>), Value, Range<CodePos>), ParserError> {
         let token_for_name = self
             .lexer
             .next()
@@ -165,10 +179,10 @@ where
                 token_for_colon.range.start,
             )),
         }?;
-        let value = self.parse_value().map_err(|e| match e {
+        let (value, last_token_range) = self.parse_value().map_err(|e| match e {
             ParserError::NoToken => ParserError::ObjectMemberLacksValue(begin_object_token_range.start, self.lexer.position()),
             _ => e,
         })?;
-        Ok(((name, token_for_name.range), value))
+        Ok(((name, token_for_name.range), value, last_token_range))
     }
 }
