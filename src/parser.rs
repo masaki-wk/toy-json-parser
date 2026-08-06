b3ad5864dc73db5356c6fdaceb25f7025d4367c3
Masaki Wakabayashi <masaki.wakabayashi@gmail.com>
2026-08-06 09:27:41 +0900

    Use named parameter instead of positional parameter in fmt for ParseError

--- src/parser.rs
+++ src/parser.rs
@@ -18,7 +18,7 @@ pub enum ParseError {
 
 impl std::fmt::Display for ParseError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{:?}", self)
+        write!(f, "{self:?}")
     }
 }
 
