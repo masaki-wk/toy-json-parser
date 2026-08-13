fd35ff7b3250a17337f0cd83ba892e53ded7ff7c
Masaki Wakabayashi <masaki.wakabayashi@gmail.com>
2026-08-13 21:34:01 +0900

    Move unit tests for Parser from tests/ to src/

--- src/parser.rs
+++ src/parser.rs
@@ -225,3 +225,334 @@ where
         Ok(((name, token_for_name.span), value, last_token_span))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::Lexer;
+    use anyhow::{Result, bail};
+
+    fn do_parse_legal_code(input: &str, expected: Value) -> Result<()> {
+        let lexer = Lexer::new(input.chars());
+        let mut parser = Parser::new(lexer);
+        let value = parser.parse()?;
+        assert_eq!(value, expected);
+        Ok(())
+    }
+
+    fn do_parse_illegal_code(input: &str, expected: ParseError) -> Result<()> {
+        let lexer = Lexer::new(input.chars());
+        let mut parser = Parser::new(lexer);
+        match parser.parse() {
+            Ok(_) => {
+                bail!("");
+            }
+            Err(e) => match e {
+                ParseError::NestingDepthExceeded(_) => {}
+                _ => {
+                    assert_eq!(e, expected);
+                }
+            },
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn new() -> Result<()> {
+        let lexer = Lexer::new("".chars());
+        let _parser = Parser::new(lexer);
+        Ok(())
+    }
+
+    #[test]
+    fn parse_number() -> Result<()> {
+        let input = "123";
+        let start = CodeLocation::new(1, 1);
+        let end = CodeLocation::new(start.line, start.column + input.chars().count());
+        let kind = ValueKind::Literal(Literal::Number(input.to_string()));
+        let span = CodeSpan::new(start, end);
+        do_parse_legal_code(input, Value::new(kind, span))
+    }
+
+    #[test]
+    fn parse_string() -> Result<()> {
+        let input = "foo";
+        let code = format!(r#""{input}""#);
+        let start = CodeLocation::new(1, 1);
+        let end = CodeLocation::new(start.line, start.column + code.chars().count());
+        let kind = ValueKind::Literal(Literal::String(input.to_string()));
+        let span = CodeSpan::new(start, end);
+        do_parse_legal_code(&code, Value::new(kind, span))
+    }
+
+    #[test]
+    fn parse_true() -> Result<()> {
+        let input = "true";
+        let start = CodeLocation::new(1, 1);
+        let end = CodeLocation::new(start.line, start.column + input.chars().count());
+        let kind = ValueKind::Literal(Literal::Boolean(true));
+        let span = CodeSpan::new(start, end);
+        do_parse_legal_code(input, Value::new(kind, span))
+    }
+
+    #[test]
+    fn parse_false() -> Result<()> {
+        let input = "false";
+        let start = CodeLocation::new(1, 1);
+        let end = CodeLocation::new(start.line, start.column + input.chars().count());
+        let kind = ValueKind::Literal(Literal::Boolean(false));
+        let span = CodeSpan::new(start, end);
+        do_parse_legal_code(input, Value::new(kind, span))
+    }
+
+    #[test]
+    fn parse_null() -> Result<()> {
+        let input = "null";
+        let start = CodeLocation::new(1, 1);
+        let end = CodeLocation::new(start.line, start.column + input.chars().count());
+        let kind = ValueKind::Literal(Literal::Null);
+        let span = CodeSpan::new(start, end);
+        do_parse_legal_code(input, Value::new(kind, span))
+    }
+
+    #[test]
+    fn parse_array_empty() -> Result<()> {
+        let input = "[]";
+        let start = CodeLocation::new(1, 1);
+        let end = CodeLocation::new(start.line, start.column + input.chars().count());
+        let buf = Vec::new();
+        let kind = ValueKind::Array(buf);
+        let span = CodeSpan::new(start, end);
+        do_parse_legal_code(input, Value::new(kind, span))
+    }
+
+    #[test]
+    fn parse_array_single_item() -> Result<()> {
+        let input = "[0]";
+        let start = CodeLocation::new(1, 1);
+        let literal_start = CodeLocation::new(start.line, start.column + 1);
+        let literal_end = CodeLocation::new(literal_start.line, literal_start.column + 1);
+        let end = CodeLocation::new(start.line, start.column + input.chars().count());
+        let buf = vec![Box::new(Value::new(
+            ValueKind::Literal(Literal::Number("0".to_string())),
+            CodeSpan::new(literal_start, literal_end),
+        ))];
+        let kind = ValueKind::Array(buf);
+        let span = CodeSpan::new(start, end);
+        do_parse_legal_code(input, Value::new(kind, span))
+    }
+
+    #[test]
+    fn parse_array_multiple_item() -> Result<()> {
+        let input = "[0, 1]";
+        let start = CodeLocation::new(1, 1);
+        let literal1_start = CodeLocation::new(start.line, start.column + 1);
+        let literal1_end = CodeLocation::new(literal1_start.line, literal1_start.column + 1);
+        let literal2_start = CodeLocation::new(literal1_end.line, literal1_end.column + 2);
+        let literal2_end = CodeLocation::new(literal2_start.line, literal2_start.column + 1);
+        let end = CodeLocation::new(start.line, start.column + input.chars().count());
+        let buf = vec![
+            Box::new(Value::new(
+                ValueKind::Literal(Literal::Number("0".to_string())),
+                CodeSpan::new(literal1_start, literal1_end),
+            )),
+            Box::new(Value::new(
+                ValueKind::Literal(Literal::Number("1".to_string())),
+                CodeSpan::new(literal2_start, literal2_end),
+            )),
+        ];
+        let kind = ValueKind::Array(buf);
+        let span = CodeSpan::new(start, end);
+        do_parse_legal_code(input, Value::new(kind, span))
+    }
+
+    #[test]
+    fn parse_object_empty() -> Result<()> {
+        let input = "{}";
+        let start = CodeLocation::new(1, 1);
+        let end = CodeLocation::new(start.line, start.column + input.chars().count());
+        let buf = Vec::new();
+        let kind = ValueKind::Object(buf);
+        let span = CodeSpan::new(start, end);
+        do_parse_legal_code(input, Value::new(kind, span))
+    }
+
+    #[test]
+    fn parse_object_single_pair() -> Result<()> {
+        let input = r#"{"a": 0}"#;
+        let start = CodeLocation::new(1, 1);
+        let name_start = CodeLocation::new(start.line, start.column + 1);
+        let name_end = CodeLocation::new(name_start.line, name_start.column + 3);
+        let value_start = CodeLocation::new(name_end.line, name_end.column + 2);
+        let value_end = CodeLocation::new(value_start.line, value_start.column + 1);
+        let end = CodeLocation::new(start.line, start.column + input.chars().count());
+        let buf = vec![(
+            ("a".to_string(), CodeSpan::new(name_start, name_end)),
+            Box::new(Value::new(
+                ValueKind::Literal(Literal::Number("0".to_string())),
+                CodeSpan::new(value_start, value_end),
+            )),
+        )];
+        let kind = ValueKind::Object(buf);
+        let span = CodeSpan::new(start, end);
+        do_parse_legal_code(input, Value::new(kind, span))
+    }
+
+    #[test]
+    fn parse_object_multiple_pair() -> Result<()> {
+        let input = r#"{"a": 0, "b": 1}"#;
+        let start = CodeLocation::new(1, 1);
+        let name1_start = CodeLocation::new(start.line, start.column + 1);
+        let name1_end = CodeLocation::new(name1_start.line, name1_start.column + 3);
+        let value1_start = CodeLocation::new(name1_end.line, name1_end.column + 2);
+        let value1_end = CodeLocation::new(value1_start.line, value1_start.column + 1);
+        let name2_start = CodeLocation::new(value1_end.line, value1_end.column + 2);
+        let name2_end = CodeLocation::new(name2_start.line, name2_start.column + 3);
+        let value2_start = CodeLocation::new(name2_end.line, name2_end.column + 2);
+        let value2_end = CodeLocation::new(value2_start.line, value2_start.column + 1);
+        let end = CodeLocation::new(start.line, start.column + input.chars().count());
+        let buf = vec![
+            (
+                ("a".to_string(), CodeSpan::new(name1_start, name1_end)),
+                Box::new(Value::new(
+                    ValueKind::Literal(Literal::Number("0".to_string())),
+                    CodeSpan::new(value1_start, value1_end),
+                )),
+            ),
+            (
+                ("b".to_string(), CodeSpan::new(name2_start, name2_end)),
+                Box::new(Value::new(
+                    ValueKind::Literal(Literal::Number("1".to_string())),
+                    CodeSpan::new(value2_start, value2_end),
+                )),
+            ),
+        ];
+        let kind = ValueKind::Object(buf);
+        let span = CodeSpan::new(start, end);
+        do_parse_legal_code(input, Value::new(kind, span))
+    }
+
+    #[test]
+    fn parse_illegal_no_token() -> Result<()> {
+        let input = "";
+        do_parse_illegal_code(input, ParseError::NoToken)
+    }
+
+    #[test]
+    fn parse_illegal_invalid_token() -> Result<()> {
+        let input = "_";
+        let loc = CodeLocation::new(1, 1);
+        do_parse_illegal_code(input, ParseError::InvalidToken(input.to_string(), loc))
+    }
+
+    #[test]
+    fn parse_illegal_delimiter_in_wrong_place() -> Result<()> {
+        let input = ",";
+        let loc = CodeLocation::new(1, 1);
+        do_parse_illegal_code(input, ParseError::DelimiterInWrongPlace(Delimiter::Comma, loc))
+    }
+
+    #[test]
+    fn parse_illegal_extra_token_at_the_end() -> Result<()> {
+        let body = "0 ";
+        let extra = "1";
+        let input = &format!("{body}{extra}");
+        let loc = CodeLocation::new(1, 1 + body.chars().count());
+        do_parse_illegal_code(input, ParseError::ExtraTokenAtTheEnd(loc))
+    }
+
+    #[test]
+    fn parse_illegal_unfinished_array_no_value() -> Result<()> {
+        let input = "[";
+        let start = CodeLocation::new(1, 1);
+        let end = CodeLocation::new(start.line, start.column + input.chars().count());
+        do_parse_illegal_code(input, ParseError::UnfinishedArray(start, end))
+    }
+
+    #[test]
+    fn parse_illegal_unfinished_array_lacks_next_comma() -> Result<()> {
+        let input = "[0";
+        let start = CodeLocation::new(1, 1);
+        let end = CodeLocation::new(start.line, start.column + input.chars().count());
+        do_parse_illegal_code(input, ParseError::UnfinishedArray(start, end))
+    }
+
+    #[test]
+    fn parse_illegal_unfinished_array_lacks_next_value() -> Result<()> {
+        let input = "[0,";
+        let start = CodeLocation::new(1, 1);
+        let end = CodeLocation::new(start.line, start.column + input.chars().count());
+        do_parse_illegal_code(input, ParseError::UnfinishedArray(start, end))
+    }
+
+    #[test]
+    fn parse_illegal_array_lacks_separator() -> Result<()> {
+        let pre = "[0";
+        let post = " 1]";
+        let input = &format!("{pre}{post}");
+        let start = CodeLocation::new(1, 1);
+        let end = CodeLocation::new(start.line, start.column + pre.chars().count());
+        do_parse_illegal_code(input, ParseError::ArrayLacksSeparator(start, end))
+    }
+
+    #[test]
+    fn parse_illegal_unfinished_object_no_name() -> Result<()> {
+        let input = "{";
+        let start = CodeLocation::new(1, 1);
+        let end = CodeLocation::new(start.line, start.column + input.chars().count());
+        do_parse_illegal_code(input, ParseError::UnfinishedObject(start, end))
+    }
+
+    #[test]
+    fn parse_illegal_unfinished_object_lacks_next_colon() -> Result<()> {
+        let input = r#"{"foo""#;
+        let start = CodeLocation::new(1, 1);
+        let end = CodeLocation::new(start.line, start.column + input.chars().count());
+        do_parse_illegal_code(input, ParseError::ObjectMemberLacksSeparator(start, end))
+    }
+
+    #[test]
+    fn parse_illegal_unfinished_object_lacks_next_value() -> Result<()> {
+        let input = r#"{"foo":"#;
+        let start = CodeLocation::new(1, 1);
+        let end = CodeLocation::new(start.line, start.column + input.chars().count());
+        do_parse_illegal_code(input, ParseError::ObjectMemberLacksValue(start, end))
+    }
+
+    #[test]
+    fn parse_illegal_unfinished_object_lacks_next_comma() -> Result<()> {
+        let input = r#"{"foo": 0"#;
+        let start = CodeLocation::new(1, 1);
+        let end = CodeLocation::new(start.line, start.column + input.chars().count());
+        do_parse_illegal_code(input, ParseError::UnfinishedObject(start, end))
+    }
+
+    #[test]
+    fn parse_illegal_object_lacks_separator() -> Result<()> {
+        let pre = r#"{"foo": 0"#;
+        let post = r#" "bar": 1}"#;
+        let input = &format!("{pre}{post}");
+        let start = CodeLocation::new(1, 1);
+        let end = CodeLocation::new(start.line, start.column + pre.chars().count());
+        do_parse_illegal_code(input, ParseError::ObjectLacksSeparator(start, end))
+    }
+
+    #[test]
+    fn parse_illegal_object_name_is_not_string() -> Result<()> {
+        let pre = "{";
+        let name = "0";
+        let post = ": 0}";
+        let input = &format!("{pre}{name}{post}");
+        let start = CodeLocation::new(1, 1);
+        let end = CodeLocation::new(start.line, start.column + pre.chars().count());
+        do_parse_illegal_code(input, ParseError::NameOfObjectMemberIsNotString(Literal::Number(name.to_string()), end))
+    }
+
+    #[test]
+    fn parse_illegal_nesting_depth_exceeded() -> Result<()> {
+        let nesting_depth = 1024;
+        let input = &format!("{}{}", "[".repeat(nesting_depth), "]".repeat(nesting_depth));
+        let dummy = CodeLocation::new(1, 1);
+        do_parse_illegal_code(input, ParseError::NestingDepthExceeded(dummy))
+    }
+}
