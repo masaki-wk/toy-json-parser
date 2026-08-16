95f8010dcb17d0409a9531a45ef29458e92f348b
Masaki Wakabayashi <masaki.wakabayashi@gmail.com>
2026-08-16 18:27:00 +0900

    Refactor parser test macros to simplify test descriptions

--- tests/test.rs
+++ tests/test.rs
@@ -1,13 +1,14 @@
 // Tests with the test suite from [JSON_checker](https://json.org/JSON_checker/)
 
 use anyhow::{Result, ensure};
+use paste::paste;
 use std::fs::File;
 use std::io::BufReader;
+use std::path::Path;
 use toy_json_parser::{BufReadCharsExt as _, Lexer, Parser, TokenKind};
 
 fn do_parser_test(filename: &str, expected_pasing_result: bool, contains_invalid_token: bool) -> Result<()> {
-    let path = format!("tests/json_checker/{filename}");
-    let file = File::open(path)?;
+    let file = File::open(Path::new(filename))?;
     let reader = BufReader::new(file);
     let lexer = Lexer::new(reader.chars());
     let tokens: Vec<_> = lexer.collect();
@@ -18,68 +19,64 @@ fn do_parser_test(filename: &str, expected_pasing_result: bool, contains_invalid
     Ok(())
 }
 
-macro_rules! generate_parser_accept_pattern_tests {
-    ($($testname:ident: $filename:expr, $contains_invalid_token:expr),* $(,)?) => {
+macro_rules! generate_parser_tests {
+    (
+        $prefix:ident, $expected_parsing_result:expr; $(
+            $basefilename:ident: $description:ident, $contains_invalid_token:expr
+        ),* $(,)?) => {
         $(
-            #[test]
-            fn $testname() -> Result<()> {
-                do_parser_test($filename, true, $contains_invalid_token)
+            paste! {
+                #[test]
+                fn [<$prefix _ $basefilename _ $description>]() -> Result<()> {
+                    do_parser_test(
+                        concat!("tests/json_checker/", stringify!($basefilename), ".json"),
+                        $expected_parsing_result,
+                        $contains_invalid_token
+                    )
+                }
             }
         )+
     };
 }
 
-macro_rules! generate_parser_deny_pattern_tests {
-    ($($testname:ident: $filename:expr, $contains_invalid_token:expr),* $(,)?) => {
-        $(
-            #[test]
-            fn $testname() -> Result<()> {
-                do_parser_test($filename, false, $contains_invalid_token)
-            }
-        )+
-    };
+generate_parser_tests! {parser_accept_pattern, true;
+    pass1:  pattern,                   false,
+    pass2:  pattern,                   false,
+    pass3:  pattern,                   false,
+    fail1:  only_one_string_but_regal, false,
+    fail18: too_deep_but_regal,        false,
 }
 
-#[rustfmt::skip]
-generate_parser_accept_pattern_tests! {
-    parser_accept_pattern_pass1:                 "pass1.json",  false,
-    parser_accept_pattern_pass2:                 "pass2.json",  false,
-    parser_accept_pattern_pass3:                 "pass3.json",  false,
-    parser_accept_pattern_fail1_only_one_string: "fail1.json",  false,
-    parser_accept_pattern_fail18_too_deep:       "fail18.json", false,
-}
-
-#[rustfmt::skip]
-generate_parser_deny_pattern_tests! {
-    parser_deny_pattern_fail2_unclosed_array:                      "fail2.json",  false,
-    parser_deny_pattern_fail3_unquoted_key:                        "fail3.json",  true,
-    parser_deny_pattern_fail4_extra_comma_in_array:                "fail4.json",  false,
-    parser_deny_pattern_fail5_double_extra_comma:                  "fail5.json",  false,
-    parser_deny_pattern_fail6_array_missing_value:                 "fail6.json",  false,
-    parser_deny_pattern_fail7_comma_after_the_close_array:         "fail7.json",  false,
-    parser_deny_pattern_fail8_extra_close_after_array:             "fail8.json",  false,
-    parser_deny_pattern_fail9_extra_comma_in_object:               "fail9.json",  false,
-    parser_deny_pattern_fail10_extra_value_after_the_close_object: "fail10.json", false,
-    parser_deny_pattern_fail11_illegal_expression:                 "fail11.json", true,
-    parser_deny_pattern_fail12_illegal_invocation:                 "fail12.json", true,
-    parser_deny_pattern_fail13_numbers_cannot_have_leading_zeroes: "fail13.json", true,
-    parser_deny_pattern_fail14_numbers_cannot_have_hex:            "fail14.json", true,
-    parser_deny_pattern_fail15_illegal_backslash_escape:           "fail15.json", true,
-    parser_deny_pattern_fail16_naked_backslash:                    "fail16.json", true,
-    parser_deny_pattern_fail17_illegal_backslash_escape:           "fail17.json", true,
-    parser_deny_pattern_fail19_missing_colon:                      "fail19.json", false,
-    parser_deny_pattern_fail20_double_colon:                       "fail20.json", false,
-    parser_deny_pattern_fail21_comma_instead_of_colon:             "fail21.json", false,
-    parser_deny_pattern_fail22_colon_instead_of_comma:             "fail22.json", false,
-    parser_deny_pattern_fail23_bad_value:                          "fail23.json", true,
-    parser_deny_pattern_fail24_single_quote:                       "fail24.json", true,
-    parser_deny_pattern_fail25_tab_character_in_string:            "fail25.json", true,
-    parser_deny_pattern_fail26_tab_character_in_string:            "fail26.json", true,
-    parser_deny_pattern_fail27_line_break_in_string:               "fail27.json", true,
-    parser_deny_pattern_fail28_line_break_in_string:               "fail28.json", true,
-    parser_deny_pattern_fail29_bad_number_0e:                      "fail29.json", true,
-    parser_deny_pattern_fail30_bad_number_0e_plus:                 "fail30.json", true,
-    parser_deny_pattern_fail31_bad_number_0e_plus_minus_1:         "fail31.json", true,
-    parser_deny_pattern_fail32_comma_instead_of_closing_brace:     "fail32.json", false,
-    parser_deny_pattern_fail33_mismatch_closing_delimiter:         "fail33.json", false,
+generate_parser_tests! {parser_deny_pattern, false;
+    fail2:  unclosed_array,                     false,
+    fail3:  unquoted_key,                       true,
+    fail4:  extra_comma_in_array,               false,
+    fail5:  double_extra_comma,                 false,
+    fail6:  array_missing_value,                false,
+    fail7:  comma_after_the_close_array,        false,
+    fail8:  extra_close_after_array,            false,
+    fail9:  extra_comma_in_object,              false,
+    fail10: extra_value_after_the_close_object, false,
+    fail11: illegal_expression,                 true,
+    fail12: illegal_invocation,                 true,
+    fail13: numbers_cannot_have_leading_zeroes, true,
+    fail14: numbers_cannot_have_hex,            true,
+    fail15: illegal_backslash_escape,           true,
+    fail16: naked_backslash,                    true,
+    fail17: illegal_backslash_escape,           true,
+    fail19: missing_colon,                      false,
+    fail20: double_colon,                       false,
+    fail21: comma_instead_of_colon,             false,
+    fail22: colon_instead_of_comma,             false,
+    fail23: bad_value,                          true,
+    fail24: single_quote,                       true,
+    fail25: tab_character_in_string,            true,
+    fail26: tab_character_in_string,            true,
+    fail27: line_break_in_string,               true,
+    fail28: line_break_in_string,               true,
+    fail29: bad_number_0e,                      true,
+    fail30: bad_number_0e_plus,                 true,
+    fail31: bad_number_0e_plus_minus_1,         true,
+    fail32: comma_instead_of_closing_brace,     false,
+    fail33: mismatch_closing_delimiter,         false,
 }
