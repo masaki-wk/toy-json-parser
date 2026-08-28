4fa8b6d79e3048d60ad70f60497534049ff3aef8
Masaki Wakabayashi <masaki.wakabayashi@gmail.com>
2026-08-28 09:32:21 +0900

    Support FILE argument by fix-readme.sh

--- .github/fix-readme.sh
+++ .github/fix-readme.sh
@@ -3,4 +3,4 @@
 # - Remove links: e.g. [`FOO`] into `FOO`
 # - Convert opening ``` of bare code blocks into ```text
 #
-sed -e 's/\[\(`[^`]*`\)\]/\1/g' -e '$!{N;s/^\n```$/\n```text/;P;D}'
+sed -e 's/\[\(`[^`]*`\)\]/\1/g' -e '$!{N;s/^\n```$/\n```text/;P;D}' $*
